b70731fb3a56eaf8fed6685f7e64f91910a21784
vatseek <vatseek13@rambler.ru>
2012-09-27 11:29:10 +0300

    install opencart

---
+++ catalog/language/russian/payment/google_checkout.php
@@ -0,0 +1,6 @@
+<?php
+// Entry
+$_['entry_postcode'] = 'Почтовый Индекс:';
+$_['entry_country']  = 'Страна:';
+$_['entry_zone']     = 'Регион / Область:';
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencartempty.co/');
+define('HTTP_IMAGE', 'http://opencartempty.co/image/');
+define('HTTP_ADMIN', 'http://opencartempty.co/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencartempty.co/');
+define('HTTPS_IMAGE', 'http://opencartempty.co/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencartempty/catalog/');
+define('DIR_SYSTEM', '/var/www/opencartempty/system/');
+define('DIR_DATABASE', '/var/www/opencartempty/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencartempty/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencartempty/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencartempty/system/config/');
+define('DIR_IMAGE', '/var/www/opencartempty/image/');
+define('DIR_CACHE', '/var/www/opencartempty/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencartempty/download/');
+define('DIR_LOGS', '/var/www/opencartempty/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartempty');
+define('DB_PASSWORD', 'opencartempty');
+define('DB_DATABASE', 'opencartempty');
+define('DB_PREFIX', '');
+?>
