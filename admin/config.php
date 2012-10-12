5ae058601911639d3dfc56e7d407ab9dd35b039a
vadim.gumennyj <vatseek13@rambler.ru>
2012-10-13 00:45:01 +0300

    Universalise config files

--- admin/config.php
+++ admin/config.php
@@ -1,32 +1,39 @@
 <?php
+$baseUrl = $_SERVER['HTTP_HOST'];
+$basePath = array_reverse(explode('/', __FILE__));
+$basePath = array_merge($basePath, array());
+unset($basePath[0]);
+unset($basePath[1]);
+$basePath = implode(array_reverse($basePath), '/');
+
 // HTTP
-define('HTTP_SERVER', 'http://opencartempty.co/admin/');
-define('HTTP_CATALOG', 'http://opencartempty.co/');
-define('HTTP_IMAGE', 'http://opencartempty.co/image/');
+define('HTTP_SERVER', 'http://' . $baseUrl . '/admin/');
+define('HTTP_CATALOG', 'http://' . $baseUrl . '/');
+define('HTTP_IMAGE', 'http://' . $baseUrl . '/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencartempty.co/admin/');
-define('HTTPS_CATALOG', 'http://opencartempty.co/');
-define('HTTPS_IMAGE', 'http://opencartempty.co/image/');
+define('HTTPS_SERVER', 'https://' . $baseUrl . '/admin/');
+define('HTTPS_CATALOG', 'https://' . $baseUrl . '/');
+define('HTTPS_IMAGE', 'https://' . $baseUrl . '/image/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/opencartempty/admin/');
-define('DIR_SYSTEM', '/var/www/opencartempty/system/');
-define('DIR_DATABASE', '/var/www/opencartempty/system/database/');
-define('DIR_LANGUAGE', '/var/www/opencartempty/admin/language/');
-define('DIR_TEMPLATE', '/var/www/opencartempty/admin/view/template/');
-define('DIR_CONFIG', '/var/www/opencartempty/system/config/');
-define('DIR_IMAGE', '/var/www/opencartempty/image/');
-define('DIR_CACHE', '/var/www/opencartempty/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/opencartempty/download/');
-define('DIR_LOGS', '/var/www/opencartempty/system/logs/');
-define('DIR_CATALOG', '/var/www/opencartempty/catalog/');
+define('DIR_APPLICATION', $basePath . '/admin/');
+define('DIR_SYSTEM', $basePath . '/system/');
+define('DIR_DATABASE', $basePath . '/system/database/');
+define('DIR_LANGUAGE', $basePath . '/admin/language/');
+define('DIR_TEMPLATE', $basePath . '/admin/view/template/');
+define('DIR_CONFIG', $basePath . '/system/config/');
+define('DIR_IMAGE', $basePath . '/image/');
+define('DIR_CACHE', $basePath . '/system/cache/');
+define('DIR_DOWNLOAD', $basePath . '/download/');
+define('DIR_LOGS', $basePath . '/system/logs/');
+define('DIR_CATALOG', $basePath . '/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'opencartempty');
-define('DB_PASSWORD', 'opencartempty');
-define('DB_DATABASE', 'opencartempty');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
 define('DB_PREFIX', '');
 ?>
--- config.php
+++ config.php
@@ -1,30 +1,38 @@
 <?php
+
+$baseUrl = $_SERVER['HTTP_HOST'];
+
+$basePath = array_reverse(explode('/', __FILE__));
+$basePath = array_merge($basePath, array());
+unset($basePath[0]);
+$basePath = implode(array_reverse($basePath), '/');
+
 // HTTP
-define('HTTP_SERVER', 'http://opencartempty.co/');
-define('HTTP_IMAGE', 'http://opencartempty.co/image/');
-define('HTTP_ADMIN', 'http://opencartempty.co/admin/');
+define('HTTP_SERVER', 'http://'.$baseUrl.'/');
+define('HTTP_IMAGE', 'http://'.$baseUrl.'/image/');
+define('HTTP_ADMIN', 'http://'.$baseUrl.'/admin/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencartempty.co/');
-define('HTTPS_IMAGE', 'http://opencartempty.co/image/');
+define('HTTPS_SERVER', 'https://'.$baseUrl.'/');
+define('HTTPS_IMAGE', 'https://'.$baseUrl.'/image/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/opencartempty/catalog/');
-define('DIR_SYSTEM', '/var/www/opencartempty/system/');
-define('DIR_DATABASE', '/var/www/opencartempty/system/database/');
-define('DIR_LANGUAGE', '/var/www/opencartempty/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/opencartempty/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/opencartempty/system/config/');
-define('DIR_IMAGE', '/var/www/opencartempty/image/');
-define('DIR_CACHE', '/var/www/opencartempty/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/opencartempty/download/');
-define('DIR_LOGS', '/var/www/opencartempty/system/logs/');
+define('DIR_APPLICATION', $basePath . '/catalog/');
+define('DIR_SYSTEM', $basePath . '/system/');
+define('DIR_DATABASE', $basePath . '/system/database/');
+define('DIR_LANGUAGE', $basePath . '/catalog/language/');
+define('DIR_TEMPLATE', $basePath . '/catalog/view/theme/');
+define('DIR_CONFIG', $basePath . '/system/config/');
+define('DIR_IMAGE', $basePath . '/image/');
+define('DIR_CACHE', $basePath . '/system/cache/');
+define('DIR_DOWNLOAD', $basePath . '/download/');
+define('DIR_LOGS', $basePath . '/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'opencartempty');
-define('DB_PASSWORD', 'opencartempty');
-define('DB_DATABASE', 'opencartempty');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
 define('DB_PREFIX', '');
 ?>
